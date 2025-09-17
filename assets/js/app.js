acbb4345e2c7f4f852fbe5da18562ec2eeffaa52
ridouanej <jaouna.ridouane@gmail.com>
2025-09-17 17:59:01 +0200

    feat(nav): ajouter la navbar statique + modal Mes participations (sans logique user)

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+import "bootstrap"; // prend bootstrap depuis node_modules
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("JS OK (esbuild)");
+    const triggers = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+    [...triggers].forEach((el) => new bootstrap.Tooltip(el));
+});
